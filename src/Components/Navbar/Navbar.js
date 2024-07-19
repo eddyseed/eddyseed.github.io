85991c5f4fb945eb30bb339ac1bc9cf04582f3d5
Eddy Seed <randomboiii069@gmail.com>
2024-07-19 12:18:01 +0530

    Fucked Up The Navbar Style

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,26 +1,31 @@
-import React from 'react'
-import './Navbar.css'
-const NAME = "RISHABH JAIN"
+import React from 'react';
+import './Navbar.css';
+
+// Define the Navbar component
 export default function Navbar() {
+
+  // Function to handle button click
+  const handleClick = () => {
+    alert("hil");
+  };
+
+  // Return JSX for the Navbar component
   return (
     <div id='navbar' className=''>
-      <div className='title-bar'>
-        <h1 className=' text-5xl'>
-          <span className='playfair-fs'>{NAME.split(" ")[0]}</span>
-          <span>{" "}</span>
-          <span className='playfair-fs'>{NAME.split(" ")[1]}</span>
-        </h1>
-      </div>
-      <div className='menu'>
-        <section className='space-x-4 font-extralight monster'>
-          <a href='/'>HOME</a>
-          <a href='/'>EXPERIENCE</a>
+      <div className='menu '>
+        <section className='space-x-4 font-extralight ubuntu-fs'>
+          <a href='/' className='active'>HOME</a>
+          <a href='/'>COFFEE CONVERTERS</a>
           <a href='/'>ABOUT</a>
         </section>
         <section className=''>
-          <i class="fa-solid fa-circle-half-stroke"></i>
+          <div onClick={handleClick}>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
         </section>
       </div>
     </div>
-  )
+  );
 }
