3477c57fb8defd9853f34a85957241c80cd13cf3
Eddy Seed <randomboiii069@gmail.com>
2024-07-25 22:21:45 +0530

    Added Basic BackBone for Navbar and Layed Elements of Mainpage

--- src/Pages/Introduction/Home.js
+++ src/Pages/Introduction/Home.js
@@ -2,6 +2,20 @@ import React from 'react'
 import './Introduction.css'
 export default function Home() {
   return (
-    <div id='HOME'>Home</div>
+    <div id='HOME' className=''>
+      <section>
+        <div></div> 
+      </section>
+      <section>
+        <div className="">
+          <a>A</a>
+          <a>B</a>
+          <a>C</a>
+        </div>
+        <div className="">
+          <button>SEE AHEAD</button>
+        </div>
+      </section>
+    </div>
   )
 }
