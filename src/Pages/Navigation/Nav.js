d7777f3574b07c6f5503cede3d047f11b3b63b6c
Eddy Seed <randomboiii069@gmail.com>
2024-07-25 17:59:30 +0530

    Added Pretty Much Of Styles and Functioning to the Navigation Menu With a Hamburger Menu Icon Coded

--- src/Pages/Navigation/Nav.js
+++ src/Pages/Navigation/Nav.js
@@ -1,18 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navigation.css'
 export default function Nav() {
+  // const [first, setfirst] = useState(second)
+  const toggleMenuVisibility = () => {
+    let menu = document.getElementById("MENU")
+    let navbar = document.getElementById("NAVBAR")
+    menu.style.display="grid"
+    navbar.style.height = "60vh"
+    navbar.style.gridTemplateRows = "1fr 6fr"
+  }
   return (
-    <div id='NAVBAR' className=''>
-      <section className="nav-links monster">
-        <a href="#" className='active'>HOME</a>
-        <a href="#">ABOUT</a>
-        <a href="#">COMMUNITY</a>
-        <a href="#">BLOG</a>
-        <a href="#">CONTACT</a>
+    <div id='NAVBAR' className='m'>
+      <section className="nav-links monster" id='MENU'>
+        <a href="https://google.com/" className='active'>HOME</a>
+        <a href="https://google.com/">ABOUT</a>
+        <a href="https://google.com/">COMMUNITY</a>
+        <a href="https://google.com/">BLOG</a>
+        <a href="https://google.com/">CONTACT</a>
       </section>
-      <section className="nav-buttons m">
-        <div className='m'>TOGG</div>
-        <div className='m'>
+      <section className="nav-buttons">
+        <div className=''>
+          <main><span className='monster font-bold text-2xl'>EDDY SEED</span></main>
+          <main className=''><button></button></main>
+        </div>
+        <div id='HAMBURGER_MENU' onClick={toggleMenuVisibility}>
           <span className='menu-part'></span>
           <span className='menu-part'></span>
           <span className='menu-part'></span>
