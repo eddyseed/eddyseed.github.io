b9b01970843fca9098f4e2b9208922aaceddc235
Eddy Seed <randomboiii069@gmail.com>
2024-07-25 18:23:43 +0530

    Add Proper Logic to Hamburger Menu

--- src/Pages/Navigation/Nav.js
+++ src/Pages/Navigation/Nav.js
@@ -1,34 +1,63 @@
-import React, { useState } from 'react'
-import './Navigation.css'
-export default function Nav() {
-  // const [first, setfirst] = useState(second)
-  const toggleMenuVisibility = () => {
-    let menu = document.getElementById("MENU")
-    let navbar = document.getElementById("NAVBAR")
-    menu.style.display="grid"
-    navbar.style.height = "60vh"
-    navbar.style.gridTemplateRows = "1fr 6fr"
+import React, { Component } from 'react';
+import './Navigation.css';
+
+class Nav extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visibility: false
+    };
   }
-  return (
-    <div id='NAVBAR' className='m'>
-      <section className="nav-links monster" id='MENU'>
-        <a href="https://google.com/" className='active'>HOME</a>
-        <a href="https://google.com/">ABOUT</a>
-        <a href="https://google.com/">COMMUNITY</a>
-        <a href="https://google.com/">BLOG</a>
-        <a href="https://google.com/">CONTACT</a>
-      </section>
-      <section className="nav-buttons">
-        <div className=''>
-          <main><span className='monster font-bold text-2xl'>EDDY SEED</span></main>
-          <main className=''><button></button></main>
-        </div>
-        <div id='HAMBURGER_MENU' onClick={toggleMenuVisibility}>
-          <span className='menu-part'></span>
-          <span className='menu-part'></span>
-          <span className='menu-part'></span>
-        </div>
-      </section>
-    </div>
-  )
+
+  componentDidMount() {
+    this.updateMenuVisibility();
+  }
+
+  updateMenuVisibility = () => {
+    let menu = document.getElementById("MENU");
+    let navbar = document.getElementById("NAVBAR");
+    
+    if (this.state.visibility) {
+      menu.style.display = "flex";
+      navbar.style.height = "60vh";
+      navbar.style.gridTemplateRows = "1fr 5fr";
+    } else {
+      menu.style.display = "none";
+      navbar.style.height = "10vh";
+      navbar.style.gridTemplateRows = "1fr 0";
+    }
+  }
+
+  toggleMenuVisibility = () => {
+    this.setState(prevState => ({
+      visibility: !prevState.visibility
+    }), this.updateMenuVisibility);
+  }
+
+  render() {
+    return (
+      <div id='NAVBAR' className=''>
+        <section className="nav-links monster" id='MENU'>
+          <a href="https://google.com/" className='active'>HOME</a>
+          <a href="https://google.com/">ABOUT</a>
+          <a href="https://google.com/">COMMUNITY</a>
+          <a href="https://google.com/">BLOG</a>
+          <a href="https://google.com/">CONTACT</a>
+        </section>
+        <section className="nav-buttons">
+          <div className=''>
+            <main><span className='monster font-bold text-2xl'>EDDY SEED</span></main>
+            <main className=''><button></button></main>
+          </div>
+          <div id='HAMBURGER_MENU' onClick={this.toggleMenuVisibility}>
+            <span className='menu-part'></span>
+            <span className='menu-part'></span>
+            <span className='menu-part'></span>
+          </div>
+        </section>
+      </div>
+    );
+  }
 }
+
+export default Nav;
