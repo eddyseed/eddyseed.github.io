eee0d85eb02112598fba1323c5e681d60ab981a2
Eddy Seed <randomboiii069@gmail.com>
2024-07-27 19:47:59 +0530

    Add Good Color Theme and BEtter Functionality

--- src/Pages/Introduction/Home.js
+++ src/Pages/Introduction/Home.js
@@ -1,28 +1,39 @@
-import React from 'react'
-import './Introduction.css'
+import React from 'react';
+import './Introduction.css';
+
 export default function Home() {
   return (
-    <div id='HOME' className=''>
+    <div id='HOME'>
       <section>
         <div>
-          <section className=''>
-            <header className='bebas-neue-regular self-end'>EDDY SEED</header>
+          <section>
+            <header className='expletus-sans self-end'>EDDY SEED</header>
           </section>
-          <section className=''>
-            <header className=' monster text-lg'>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College</header>
+          <section>
+            <header className='self-start monster text-lg'>
+              Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College
+            </header>
           </section>
         </div>
       </section>
       <section>
-        <div className="">
-          <a><i class="fa-brands fa-discord"></i></a>
-          <a><i class="fa-brands fa-github-alt"></i></a>
-          <a><i class="fa-brands fa-stack-overflow"></i></a>
+        <div>
+          <a href="https://discord.gg/YWGy27S6uj" target="_blank" rel="noopener noreferrer">
+            <i className="fa-brands fa-discord"></i>
+          </a>
+          <a href="https://github.com/eddyseed" target="_blank" rel="noopener noreferrer">
+            <i className="fa-brands fa-github-alt"></i>
+          </a>
+          <a href="https://stackoverflow.com/users/21011196/rishabh-jain" target="_blank" rel="noopener noreferrer">
+            <i className="fa-brands fa-stack-overflow"></i>
+          </a>
         </div>
-        <div className="">
-          <button>SEE AHEAD</button>
+        <div>
+          <button>
+            <span>SEE AHEAD</span>
+          </button>
         </div>
       </section>
     </div>
-  )
+  );
 }
---
+++ src/Pages/Navigation/Linkage.js
@@ -0,0 +1,9 @@
+export const NAV_LINKS = [
+    { href: "https://google.com/", text: "HOME" },
+    { href: "https://google.com/", text: "ABOUT" },
+    { href: "https://google.com/", text: "COMMUNITY" },
+    { href: "https://google.com/", text: "BLOG" },
+    { href: "https://google.com/", text: "CONTACT" }
+  ];
+  
+  export const SITE_HEADER = "EDDY 127";
