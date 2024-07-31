10aba477273506aa9d31ffedfdc6ce0689123bb0
Eddy Seed <randomboiii069@gmail.com>
2024-07-31 23:16:44 +0530

    Add Main Introduction Page

--- src/Pages/Introduction/Main.js
+++ src/Pages/Introduction/Main.js
@@ -2,6 +2,22 @@ import React from 'react'
 import './Introduction.css'
 export default function Main() {
   return (
-    <div id='MAIN'>Introduction Page</div>
+    <div id='MAIN' className=''>
+      <section className=''>
+        <div>
+          <span className='monster'>Who Am I?</span>
+        </div>
+        <div>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla, quia? Tempora beatae quia praesentium, reprehenderit numquam minus non. Dignissimos, quaerat repellat expedita quisquam dolore nihil architecto id sit alias doloribus dicta exercitationem, culpa quo ullam porro, quidem totam distinctio dolor nostrum reiciendis soluta non voluptates reprehenderit suscipit. Non delectus eum facere dicta, nostrum inventore error, numquam libero commodi, quibusdam possimus voluptates. Quidem impedit ratione asperiores, voluptatem fuga eius facilis quia!
+        </div>
+        <div>
+          <button><span>VIEW RESUME</span></button>
+        </div>
+      </section>
+      <section>
+        <div>
+        </div>
+      </section>
+    </div>
   )
 }
