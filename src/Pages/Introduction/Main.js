21161a0c331150bcf140f44b5b2028b63c25d4b0
Eddy Seed <randomboiii069@gmail.com>
2024-09-16 10:19:12 +0530

    Removed Main Page (Unstable Design)

--- src/Pages/Introduction/Main.js
+++ src/Pages/Introduction/Main.js
@@ -1,24 +1,7 @@
 import React from 'react'
-import './Introduction.css'
+
 export default function Main() {
   return (
-    <div id='MAIN' className=''>
-      <section className=''>
-        <div>
-          <span className='monster'>Who Am I?</span>
-        </div>
-        <div>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla, quia? Tempora beatae quia praesentium, reprehenderit numquam minus non. Dignissimos, quaerat repellat expedita quisquam dolore nihil architecto id sit alias doloribus dicta exercitationem, culpa quo ullam porro, quidem totam distinctio dolor nostrum reiciendis soluta non voluptates reprehenderit suscipit. Non delectus eum facere dicta, nostrum inventore error, numquam libero commodi, quibusdam possimus voluptates. Quidem impedit ratione asperiores, voluptatem fuga eius facilis quia!
-        </div>
-        <div>
-          <button><span>VIEW RESUME</span></button>
-        </div>
-      </section>
-      <section>
-        <div>
-          <img src="https://media.discordapp.net/attachments/1151430705784561674/1268484601127632947/download.png?ex=66ac97d8&is=66ab4658&hm=530c10b6d2495cc7831af6974cac94272b426e1a50a2a0f2f7508d28c66dcab5&=&format=webp&quality=lossless" alt="" />
-        </div>
-      </section>
-    </div>
+    <div>Main</div>
   )
 }
